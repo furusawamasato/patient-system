212814e7cbb7456a8b3ecdec9ec3c59cf7fa98cf
furusawamasato <furusawama@cy-com.co.jp>
2025-06-19 11:21:32 +0900

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,5 +10,12 @@ INSERT INTO patients (list_id, name, age, bloodType, diseaseName, symptoms, late
   (3, '徳川家康', 73, 'AB', '風邪', '発熱、喉の痛み', '2025-06-03', '治療室'),
   (4, '武田信玄', 45, 'A', '喘息', '呼吸困難', '2025-06-02', '呼吸器科');
 
+INSERT INTO bookingLists (treatmentDetails, bookingDate, bookingTime, place, id) VALUES
+    ('ああああ', '2025-06-02', '00:00', 'A室', 1),
+    ('ああああ', '2025-07-02', '00:00', 'A室', 2),
+    ('ああああ', '2025-08-02', '00:00', 'A室', 3),
+    ('ああああ', '2025-09-02', '00:00', 'A室', 4);
+
+
 INSERT INTO users (username, password) VALUES
   ('sample', '$2a$08$hBocxV325GV9RKzHR.lHreJr8DsMrkg6vg0kRTbNnBvUFknu670GC');
